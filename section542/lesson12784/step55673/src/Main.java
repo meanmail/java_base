ac856a4b93a853d43816347d11074bf184f71bde
meanmail <meanmail@mail.ru>
2017-02-04 18:01:46 +0700

    Added step12 char stream tests

--- section542/lesson12784/step55673/src/Main.java
+++ section542/lesson12784/step55673/src/Main.java
@@ -1,3 +1,12 @@
-public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-    // your implementation here
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+class Main {
+    //Stepik code: start
+    public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
+        // your implementation here
+        return "";
+    }
+//Stepik code: end
 }
---
+++ section542/lesson12784/step55673/src/MainTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author meanmail
+ */
+public class MainTest {
+    @Test(timeout = 8000)
+    public void readAsStringANSII() throws Exception {
+        ByteArrayInputStream input = new ByteArrayInputStream(new byte[]{48, 49, 50, 51});
+        String actual = Main.readAsString(input, StandardCharsets.US_ASCII);
+
+        assertEquals("0123", actual);
+    }
+
+    @Test(timeout = 8000)
+    public void readAsStringUTF16() throws Exception {
+        ByteArrayInputStream input = new ByteArrayInputStream(new byte[]{0x04, 0x10, 0x04, 0x11});
+        String actual = Main.readAsString(input, StandardCharsets.UTF_16);
+
+        assertEquals("\u0410\u0411", actual);
+    }
+}
