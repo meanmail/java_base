ead4bafd2353819a3bd174ab5cd5bd537b2a85a6
meanmail <meanmail@mail.ru>
2017-02-06 01:44:07 +0700

    Functional Interface step8 set default content in Main class

--- section541/lesson12777/step57838/src/Main.java
+++ section541/lesson12777/step57838/src/Main.java
@@ -1,8 +1,15 @@
-public static <T, U> Function<T, U> ternaryOperator(
-        Predicate<? super T> condition,
-        Function<? super T, ? extends U> ifTrue,
-        Function<? super T, ? extends U> ifFalse) {
+import java.util.function.Function;
+import java.util.function.Predicate;
 
-    return null; // your implementation here
+class Main {
+    //Stepik code: start
+    public static <T, U> Function<T, U> ternaryOperator(
+            Predicate<? super T> condition,
+            Function<? super T, ? extends U> ifTrue,
+            Function<? super T, ? extends U> ifFalse) {
 
+        return null; // your implementation here
+
+    }
+//Stepik code: end
 }
