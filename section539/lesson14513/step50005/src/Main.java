fb4325624955be39f5010d542488b9fd20853476
meanmail <meanmail@mail.ru>
2017-02-05 17:03:21 +0700

    Refactoring test Abstract classes step8

--- section539/lesson14513/step50005/src/Main.java
+++ section539/lesson14513/step50005/src/Main.java
@@ -1,20 +1,6 @@
 class Main {
     //Stepik code: start
-    public static class AsciiCharSequence /* extends/implements */ {
-        public AsciiCharSequence(byte[] bytes) {
-        }
-
-        public int length() {
-            return 0;
-        }
-
-        public char charAt(int i) {
-            return 0;
-        }
-
-        public CharSequence subSequence(int start, int end) {
-            return null;
-        }
+    public class AsciiCharSequence /* extends/implements */ {
         // implementation
     }
 //Stepik code: end
--- section539/lesson14513/step50005/src/MainTest.java
+++ section539/lesson14513/step50005/src/MainTest.java
@@ -1,25 +1,83 @@
 // Don't edit this file
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * @author meanmail
  */
 public class MainTest {
-    @Test
-    public void sample1() {
-        String string = "I'm string";
-        Main.AsciiCharSequence asciiCharSequence = new Main.AsciiCharSequence(string.getBytes());
+    private static Constructor<?> constructor;
+    private static Class<?> mainClass;
+    private static Method length;
+    private static Method charAt;
+    private static Method subSequence;
 
-        assertEquals("AsciiCharSequence.length()", string.length(), asciiCharSequence.length());
+    @BeforeClass
+    public static void beforeClass() {
+        mainClass = TestUtils.getUserClass("Main");
+        List<Class<?>> classes = Arrays.stream(mainClass.getDeclaredClasses())
+                .filter(clazz -> clazz.getSimpleName().equals("AsciiCharSequence"))
+                .collect(Collectors.toList());
+
+        if (classes.size() == 0) {
+            fail("Main.AsciiCharSequence did't found");
+        }
+        Class<?> asciiCharSequenceClass = classes.get(0);
+
+        constructor = TestUtils.getConstructor(asciiCharSequenceClass,
+                Modifier.PUBLIC,
+                mainClass,
+                byte[].class);
+
+        length = TestUtils.getMethod(asciiCharSequenceClass,
+                "length",
+                Modifier.PUBLIC,
+                Integer.TYPE);
+
+        charAt = TestUtils.getMethod(asciiCharSequenceClass,
+                "charAt",
+                Modifier.PUBLIC,
+                Character.TYPE,
+                Integer.TYPE);
+
+        subSequence = TestUtils.getMethod(asciiCharSequenceClass,
+                "subSequence",
+                Modifier.PUBLIC,
+                CharSequence.class,
+                Integer.TYPE,
+                Integer.TYPE);
+    }
+
+    @Test
+    public void sample1() throws IllegalAccessException, InstantiationException {
+        Object mainInstance = mainClass.newInstance();
+        String string = "I'm string";
+        Object asciiCharSequence = TestUtils.newInstance(constructor, mainInstance, string.getBytes());
+
+        assertTrue("Main.AsciiCharSequence should implement java.lang.CharSequence", asciiCharSequence instanceof CharSequence);
+
+        int actualLength = (int) TestUtils.invokeMethod(asciiCharSequence, length);
+        assertEquals("AsciiCharSequence.length()", string.length(), actualLength);
         assertEquals(string, asciiCharSequence.toString());
 
         for (int i = 0; i < string.length(); i++) {
-            assertEquals(String.format("AsciiCharSequence.charAt(%d)", i), string.charAt(i), asciiCharSequence.charAt(i));
+            char actualChar = (char) TestUtils.invokeMethod(asciiCharSequence, charAt, i);
+            assertEquals(String.format("AsciiCharSequence.charAt(%d)", i), string.charAt(i), actualChar);
         }
 
-        String substring = asciiCharSequence.subSequence(4, 8).toString();
-        assertEquals("AsciiCharSequence.substring(4, 8)", string.substring(4, 8), substring);
+        Object substring = TestUtils.invokeMethod(asciiCharSequence, subSequence, 4, 8);
+        assertNotNull("AsciiCharSequence.substring(4, 8) return null", substring);
+        assertEquals("AsciiCharSequence.substring(4, 8)", string.substring(4, 8), substring.toString());
     }
 }
